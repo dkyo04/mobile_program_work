61a9dd32d8acf82fdb3afb762b834566fe756d6a
dkyo04 <leo.k.doo@gmail.com>
2022-10-09 02:23:13 +0900

    연예인 이미지 추가

--- App.js
+++ App.js
@@ -1,22 +1,52 @@
 import React from "react";
-import { Button, View, ScrollView, StyleSheet } from "react-native";
+import { Button, View, ScrollView, StyleSheet, Text, Image, ImageBackground } from "react-native";
+import Constants from 'expo-constants';
 
 const App = () => {
   return (<>
+    <View style={{paddingTop:Constants.statusBarHeight}}></View>
     <View style={[styles.container, styles.playingSpace]}>
-
+      <Text>모바일 프로그래밍.</Text>
+      <Text>즐겁다 항상.</Text>
+      
+      <Image source={require('./assets/favicon.png')}/>
+      <ImageBackground
+        style={{width:200, height:200}}
+        source={{
+          uri: 'https://t1.daumcdn.net/cfile/tistory/99CC093D5B215BF41E',
+        }}
+      > 
+        <Text>
+          안녕 난 츄야!
+        </Text>
+      
+      </ImageBackground>
     </View>
-    <ScrollView style={[styles.container]}>
-    <View style={[styles.controlSpace]}>
-          <Button title="CHANGE FLEX DIRECTION"/>
-          <Button title="CHANGE JUSTIFY CONTENT"/>
-          <Button title="CHANGE ALIGN ITEMS"/>
-          <Button title="CHANGE DIRECTION"/>
-          <Button title="CHANGE FLEX WRAP"/>
-          <Button title="ADD SQUARE"/>
-          <Button title="DELETE SQUARE"/>
-        </View>
 
+    <ScrollView style={[styles.container]}>
+      <View style = {[styles.controlSpace]}>
+        <View style={[styles.buttonView]}>
+          <Button title="1" />
+        </View>
+        <View style={[styles.buttonView]}>
+          <Button title="2" />
+        </View>
+        <View style={[styles.buttonView]}>
+          <Button title="3" />
+        </View>
+        <View style={[styles.buttonView]}>
+          <Button title="4" />
+        </View>
+        <View style={[styles.buttonView]}>
+          <Button title="5" />
+        </View>
+        <View style={[styles.buttonView]}>
+          <Button title="6" />
+        </View>
+        <View style={[styles.buttonView]}>
+          <Button title="7" />
+        </View>
+      </View>
       
 
     </ScrollView>
